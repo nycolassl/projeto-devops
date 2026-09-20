bef74bacff189351ac0a193651d2d93748b067f3
nycolassl <nycshtml@gmail.com>
2026-09-20 15:50:45 -0300

    adiciona função de subtração

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ def somar(a, b):
     return a + b
 
 
+def subtrair(a, b):
+    return a - b
+
+
 if __name__ == "__main__":
-    resultado = somar(10, 5)
-    print(f"Resultado da soma: {resultado}")
+    resultado_soma = somar(10, 5)
+    resultado_subtracao = subtrair(10, 5)
+
+    print(f"Resultado da soma: {resultado_soma}")
+    print(f"Resultado da subtração: {resultado_subtracao}")
