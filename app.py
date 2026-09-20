81195f3bc5161867ebb39df7583b38b9ac2d821e
nycolassl <nycshtml@gmail.com>
2026-09-20 15:51:38 -0300

    adiciona função de multiplicação

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ def subtrair(a, b):
     return a - b
 
 
+def multiplicar(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     resultado_soma = somar(10, 5)
     resultado_subtracao = subtrair(10, 5)
+    resultado_multiplicacao = multiplicar(10, 5)
 
     print(f"Resultado da soma: {resultado_soma}")
     print(f"Resultado da subtração: {resultado_subtracao}")
+    print(f"Resultado da multiplicação: {resultado_multiplicacao}")
