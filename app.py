12de0fe892d6f709f0928fddcff3ff0c9e475432
nycolassl <nycshtml@gmail.com>
2026-09-20 15:53:18 -0300

    adiciona função de divisão

--- app.py
+++ app.py
@@ -10,11 +10,19 @@ def multiplicar(a, b):
     return a * b
 
 
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("Não é possível dividir por zero")
+    return a / b
+
+
 if __name__ == "__main__":
     resultado_soma = somar(10, 5)
     resultado_subtracao = subtrair(10, 5)
     resultado_multiplicacao = multiplicar(10, 5)
+    resultado_divisao = dividir(10, 5)
 
     print(f"Resultado da soma: {resultado_soma}")
     print(f"Resultado da subtração: {resultado_subtracao}")
     print(f"Resultado da multiplicação: {resultado_multiplicacao}")
+    print(f"Resultado da divisão: {resultado_divisao}")
